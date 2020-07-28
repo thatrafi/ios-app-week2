eadb2ee79cde879736fb00ef3735123118306676
that_rafi <muhammad.rafiudin@students.amikom.ac.id>
2020-07-28 17:18:00 +0700

    Initial Commit

---
+++ App Week2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App Week2
+//
+//  Created by rafiudin on 28/07/20.
+//  Copyright © 2020 rafiudin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
